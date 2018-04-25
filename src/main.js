81a5851d56360abf1bb4a4610f5d7d7549203172
Sean Holden <sean.holden@bbc.co.uk>
2018-04-25 09:18:56 +0100

    Adding settings config to main

--- src/main.js
+++ src/main.js
@@ -4,6 +4,39 @@ import { Select } from "../node_modules/genie/src/components/select.js";
 import { ClickProgressionGame } from "./components/click-progression-game.js";
 import { Results } from "../node_modules/genie/src/components/results.js";
 import { startup } from "../node_modules/genie/src/core/startup.js";
+import { settings } from "../node_modules/genie/src/core/settings.js";
+
+settings.setCloseCallback(() => {
+    //Called when settings screen has been closed
+    console.log("Settings Closed");
+});
+
+const settingsConfig = {
+    pages: [
+        {
+            title: "Global Settings",
+            settings: [
+                {
+                    key: "audio",
+                    type: "toggle",
+                    title: "Audio",
+                    description: "Turn off/on sound and music",
+                },
+                {
+                    key: "custom1",
+                    type: "toggle",
+                    title: "Custom 1",
+                    description: "Switch custom message",
+                },
+            ],
+        },
+    ],
+};
+
+settings.add("custom1", value => {
+    //Example of custom setting callback
+    console.log("custom 1 setting changed to: " + value);
+});
 
 const transitions = [
     {
