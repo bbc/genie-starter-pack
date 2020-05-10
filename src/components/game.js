ab0366def346e4e04ff142b2fbbfa12a1a27844c
Nick Tipping <nick@bigredmonster.com>
2020-05-10 15:10:30 +0100

    Update develop path

--- src/components/game.js
+++ src/components/game.js
@@ -1,7 +1,7 @@
 import { Screen } from "/node_modules/genie/src/core/screen.js";
 
 //DELETE THE LINE BELOW AND AND UNCOMMENT THE CLASS AFTERWARDS TO START!
-export { Game as Game } from "../../node_modules/genie/src/components/game.js";
+export { Game as Game } from "/node_modules/genie/src/components/game.js";
 
 //export class Game extends Screen {
 //    create() {
